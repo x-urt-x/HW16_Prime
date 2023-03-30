1cdf3d9f25466c82c2dedd877b3e4af816cf6243
urt <urt5002urt@gmail.com>
2023-03-30 22:45:43 +0300

    add all

---
+++ HW16_Prime/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Concurrent;
+
+namespace HW16_Prime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new ConcurrentBag<int>();
+            Parallel.For(0, int.Parse(Console.ReadLine()), i =>
+            {
+                if (IsPrime(i)) arr.Add(i);
+            });
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        public static bool IsPrime(int n)
+        {
+            if (n < 2)
+                return false;
+            if (n < 4)
+                return true;
+
+            for (int i = 2; i < n; i++)
+            {
+                if (n % i == 0) return false;
+            }
+            return true;
+        }
+    }
+}
